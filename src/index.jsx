d7875ceae20f7b651c5b1c770e082e41be45fbe1
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2023-07-20 03:13:48 +0530

    media query added and pngs added

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOMClient from 'react-dom/client';
 import {Wip} from './screens/Wip';
 import {Iphone} from './screens/Iphone/Iphone';
+import App from './app';
 
 const app = document.getElementById('app');
 const root = ReactDOMClient.createRoot(app);
-root.render(<Iphone />);
+root.render(<App />);
